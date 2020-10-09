674bcc9ef5d690e25d76779a2ef6ce1cc98018cc
Hiba <hiba.mashfij@gmail.com>
2020-10-09 19:19:25 +0300

    added a modal for item form

--- src/ItemList/index.js
+++ src/ItemList/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-// import { Card, Button } from "react-bootstrap";
+import React , {useState} from "react";
+import { Card, Button ,Modal } from "react-bootstrap";
 import "../index.css";
 import Item from "../Item/index.js";
 import ItemForm from "../ItemForm/index.js";
@@ -8,10 +8,38 @@ import ItemForm from "../ItemForm/index.js";
 
 // /// single card
 const ItemList = ({boardInfo}) => {
- //console.log(boardInfo.id)
+  const [show, setShow] = useState(false);
+ //console.log(boardInfo)
+ const handleClose = () => setShow(false);
+ const handleShow = () => setShow(true);
   return (
     <div>
-     <ItemForm boardItems={boardInfo.items} boardId={boardInfo.id}/>   
+      <Card className="add-card" type="button" variant="primary" onClick={handleShow}>
+        <Card.Body className="text-center">
+          <svg
+            align="center"
+            width="1.5em"
+            height="1.5em"
+            viewBox="0 0 16 16"
+            class="bi bi-plus"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleClose} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+      <Modal.Body> <ItemForm boardItems={boardInfo.items} boardId={boardInfo.id}/>  </Modal.Body>
+      <Button variant="secondary" onClick={handleClose}>
+            Close
+      </Button>
+      </Modal>
+     
     {boardInfo.items.map((item) => {
        // console.log(item)
       return <Item itemInfo={item} boardId={boardInfo.id} />;
@@ -26,25 +54,25 @@ const ItemList = ({boardInfo}) => {
 
 //   return (
 //     <div>
-//       <Card className="add-card" type="button">
-//         {/* <Card.Body className="text-center">
-//           <svg
-//             align="center"
-//             width="1.5em"
-//             height="1.5em"
-//             viewBox="0 0 16 16"
-//             class="bi bi-plus"
-//             fill="currentColor"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <path
-//               fill-rule="evenodd"
-//               d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
-//             />
-//           </svg>
-//         </Card.Body> */}
+      // <Card className="add-card" type="button">
+      //   <Card.Body className="text-center">
+      //     <svg
+      //       align="center"
+      //       width="1.5em"
+      //       height="1.5em"
+      //       viewBox="0 0 16 16"
+      //       class="bi bi-plus"
+      //       fill="currentColor"
+      //       xmlns="http://www.w3.org/2000/svg"
+      //     >
+      //       <path
+      //         fill-rule="evenodd"
+      //         d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+      //       />
+      //     </svg>
+      //   </Card.Body>
        
-//       </Card>
+      // </Card>
 //       <Card className="inner-card">
 //   <Card.Header as="h6">Name: {props.board.item.something}</Card.Header>
 //         <Card.Body>
