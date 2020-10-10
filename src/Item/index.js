de3c1ad75c8113c908124ca47ae0ca5254e50db3
khaldoon khaldi <khaldoon9kh9@gmail.com>
2020-10-10 13:02:45 +0300

    deletid nod_modules, added edite to item

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import db from "../firebaseConfig.js";
-import {Card} from "react-bootstrap";
+import {Form,Card} from "react-bootstrap";
 import "../index.css";
 import firebase from '@firebase/app';
 
 // // /// single card
 const Item = ({ itemInfo, boardId }) => {
 
-   console.log(itemInfo)
+  const [clicked,setClicked] = useState(false);
+  const [changeTitle,setChangeTitle] = useState(itemInfo.itemTitle);
+
+   //console.log(itemInfo)
 
    const handleClick = async (e) => {
     await db.collection("Board").doc(boardId).update({
@@ -17,14 +20,34 @@ const Item = ({ itemInfo, boardId }) => {
   };
 
 
+  const handleEdite =() =>{
+    setClicked(!clicked)
+    if (clicked === false){
+      setChangeTitle(itemInfo.itemTitle)
+    }else{
+      setChangeTitle(
+        <Form>
+          <input
+          type="text"
+          value={itemInfo.itemTitle}>
+          </input>
+        </Form>
+      )
+    }
+  }
+
+  
+
+
   return (
     <Card className="inner-card">
-      <Card.Header as="h6">Name: {itemInfo.itemTitle}</Card.Header>
+      <Card.Header as="h6">Name: {changeTitle}</Card.Header>
       <Card.Body>
         <Card.Text>Date: {itemInfo.date}</Card.Text>
         <Card.Text>{itemInfo.people}</Card.Text>
       </Card.Body>
       <button onClick={(e) => handleClick(e)}> Delete </button>
+      <button onClick={(e) => handleEdite(e)}> Edit </button>
     </Card>
   );
 };
